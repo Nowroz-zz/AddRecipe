181a084642948c34cafdfc5523d31b1007376348
Nowroz <isl.nowroz@gmail.com>
2022-09-02 18:07:19 +0600

    Initial Commit

---
+++ AddRecipe/AddRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddRecipeApp.swift
+//  AddRecipe
+//
+//  Created by Nowroz Islam on 2/9/22.
+//
+
+import SwiftUI
+
+@main
+struct AddRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AddRecipe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AddRecipe
+//
+//  Created by Nowroz Islam on 2/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
