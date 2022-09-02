fd3876b740a18709b470d8dd4d645c46a521a9de
Nowroz <isl.nowroz@gmail.com>
2022-09-02 20:42:02 +0600

    Create AddRecipeView File

---
+++ AddRecipe/AddRecipeView.swift
@@ -0,0 +1,20 @@
+//
+//  AddRecipeView.swift
+//  AddRecipe
+//
+//  Created by Nowroz Islam on 2/9/22.
+//
+
+import SwiftUI
+
+struct AddRecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddRecipeView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddRecipeView()
+    }
+}
