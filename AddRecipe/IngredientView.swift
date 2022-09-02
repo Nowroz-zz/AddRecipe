97fc8ea373d9256ba6fde3b5f631892317adfe94
Nowroz <isl.nowroz@gmail.com>
2022-09-02 20:54:28 +0600

    Create IngredientView File

---
+++ AddRecipe/IngredientView.swift
@@ -0,0 +1,20 @@
+//
+//  IngredientView.swift
+//  AddRecipe
+//
+//  Created by Nowroz Islam on 2/9/22.
+//
+
+import SwiftUI
+
+struct IngredientView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IngredientView_Previews: PreviewProvider {
+    static var previews: some View {
+        IngredientView()
+    }
+}
