bfa983f9e7e27aa0ca853c5dbf9a6c4cfe5bfa17
Nowroz <isl.nowroz@gmail.com>
2022-09-03 19:41:01 +0600

    Fix Typo in AddRecipeApp
    viewContext had a typo, fixed it.
    
     On branch main
     Changes to be committed:
    	modified:   AddRecipe/AddRecipeApp.swift

--- AddRecipe/AddRecipeApp.swift
+++ AddRecipe/AddRecipeApp.swift
@@ -14,7 +14,7 @@ struct AddRecipeApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, dataController.container.viewContextg)
+                .environment(\.managedObjectContext, dataController.container.viewContext)
         }
     }
 }
